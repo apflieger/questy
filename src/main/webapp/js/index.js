aeeae74fda6c3ba1728689b86e29c944d4cc0a9d
apf <loonaymail@gmail.com>
2013-11-19 21:31:13 +0000

    ng-view avec une vue login

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,4 +1,15 @@
-var questy = angular.module('questy', []);
+var questy = angular.module('questy', [ 'ngRoute' ]);
+
+questy.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/qform', {
+		templateUrl : 'qform.html'
+	}).when('/login', {
+		templateUrl : 'login.html'
+	}).otherwise({
+		redirectTo : '/qform'
+	});
+
+} ]);
 
 questy.controller('QForm', function($scope) {
 	$scope.submit = function() {
